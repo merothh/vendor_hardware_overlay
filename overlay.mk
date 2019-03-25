cd30d90e4ff20fff766f15a8b67d753d24940cc4
merothh <merothh@gmail.com>
2019-03-25 20:52:51 +0530

    overlays: bring in violet

--- overlay.mk
+++ overlay.mk
@@ -67,6 +67,7 @@ PRODUCT_PACKAGES += \
 	treble-overlay-xiaomi-redminote6pro \
 	treble-overlay-xiaomi-redminote6pro-systemui \
 	treble-overlay-xiaomi-redminote7 \
+	treble-overlay-xiaomi-redminote7pro \
 	treble-overlay-xiaomi-redmis2 \
 
 
